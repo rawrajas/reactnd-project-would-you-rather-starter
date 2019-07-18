fa04f18ee65537e0467ade6aa634e9c8114d5890
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2019-07-18 07:08:08 -0500

    feat: create Store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import reducer from './reducers';
+import middleware from './middleware';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
